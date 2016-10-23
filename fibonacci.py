5f8e8666649602a7e098c957de0a173c20937c72
hlfjane <zju_jane@163.com>
2016-10-23 16:46:32 +0800

    this is a python project to generate a fibonacci dict

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def main():
+    max_num=100
+    dict_fib=dict.fromkeys(range(100))
+    dict_fib[0],dict_fib[1] = 0, 1
+    for i in range(2,max_num):
+        dict_fib[i] = dict_fib[i-1]+dict_fib[i-2]
+    n = input("a num < 12345678:")
+    return True if n in dict_fib.values() else False
+
+main()
