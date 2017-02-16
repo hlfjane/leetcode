193193db9a74b9b37a8eb5a4c1a5b907b0e04430
hlfjane <zju_jane@163.com>
2017-02-16 23:34:51 +0800

    Update Jump_Game_II_BFS.c

--- Jump_Game_II_BFS.c
+++ Jump_Game_II_BFS.c
@@ -9,6 +9,7 @@ For example:
 Given array A = [2,3,1,1,4]
 
 The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+E.g.2,3,4,1,1,2,8,3,4--> 2/3,4/1,1,2,8/3,4
 */
 int Max(int a, int b)
 {
